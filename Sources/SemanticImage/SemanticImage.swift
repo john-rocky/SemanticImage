fb2ac2ecb0640a7abcebdbc951e0f677d9c46e9e
john-rocky <john-rocky@hubot.com>
2021-12-24 01:26:13 +0900

    saliencyMask

--- Sources/SemanticImage/SemanticImage.swift
+++ Sources/SemanticImage/SemanticImage.swift
@@ -106,6 +106,71 @@ public class SemanticImage {
         return final
     }
     
+    // MARK: Saliency
+    
+    public func saliencyMask(uiImage:UIImage) -> UIImage? {
+        let newImage = getCorrectOrientationUIImage(uiImage:uiImage)
+        guard let ciImage = CIImage(image: newImage),
+              let request = segmentationRequest else { print("Image processing failed.Please try with another image."); return nil }
+        let handler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+        do {
+            try handler.perform([request])
+            guard let result = request.results?.first as? VNPixelBufferObservation
+                   else { print("Image processing failed.Please try with another image.") ; return nil }
+            let maskCIImage = CIImage(cvPixelBuffer: result.pixelBuffer)
+            let scaledMask = maskCIImage.resize(as: CGSize(width: ciImage.extent.width, height: ciImage.extent.height))
+
+            guard let safeCGImage = ciContext.createCGImage(scaledMask, from: scaledMask.extent) else { print("Image processing failed.Please try with another image.") ; return nil }
+            let maskUIImage = UIImage(cgImage: safeCGImage)
+            return maskUIImage
+
+        } catch let error {
+            print("Vision error \(error)")
+            return nil
+        }
+    }
+    
+    public func saliencyBlend(objectUIImage:UIImage, backgroundUIImage: UIImage) -> UIImage? {
+        let newSaliencyUIImage = getCorrectOrientationUIImage(uiImage:objectUIImage)
+        let newBackgroundUIImage = getCorrectOrientationUIImage(uiImage:backgroundUIImage)
+        
+        guard let personCIImage = CIImage(image: newSaliencyUIImage),
+              let backgroundCIImage = CIImage(image: newBackgroundUIImage),
+              let maskUIImage = saliencyMask(uiImage: newSaliencyUIImage),
+              let maskCIImage = CIImage(image: maskUIImage) else {
+                  return nil }
+        
+        let backgroundImageSize = backgroundCIImage.extent
+        let originalSize = personCIImage.extent
+        var scale:CGFloat = 1
+        let widthScale =  originalSize.width / backgroundImageSize.width
+        let heightScale = originalSize.height / backgroundImageSize.height
+        if widthScale > heightScale {
+            scale = personCIImage.extent.width / backgroundImageSize.width
+        } else {
+            scale = personCIImage.extent.height / backgroundImageSize.height
+        }
+        
+        let scaledBG = backgroundCIImage.resize(as: CGSize(width: backgroundCIImage.extent.width*scale, height: backgroundCIImage.extent.height*scale))
+        let BGCenter = CGPoint(x: scaledBG.extent.width/2, y: scaledBG.extent.height/2)
+        let originalExtent = personCIImage.extent
+        let cropRect = CGRect(x: BGCenter.x-(originalExtent.width/2), y: BGCenter.y-(originalExtent.height/2), width: originalExtent.width, height: originalExtent.height)
+        let croppedBG = scaledBG.cropped(to: cropRect)
+        let translate = CGAffineTransform(translationX: -croppedBG.extent.minX, y: -croppedBG.extent.minY)
+        let traslatedBG = croppedBG.transformed(by: translate)
+        print(traslatedBG.extent)
+        guard let blended = CIFilter(name: "CIBlendWithMask", parameters: [
+            kCIInputImageKey: personCIImage,
+            kCIInputBackgroundImageKey:traslatedBG,
+            kCIInputMaskImageKey:maskCIImage])?.outputImage,
+              let safeCGImage = ciContext.createCGImage(blended, from: blended.extent) else { print("Image processing failed.Please try with another image."); return nil }
+        let blendedUIImage = UIImage(cgImage: safeCGImage)
+        return blendedUIImage
+    }
+    
+    // MARK: Rectangle
+    
+    
     public func faceRectangle(uiImage:UIImage) -> UIImage? {
         let newImage = getCorrectOrientationUIImage(uiImage:uiImage)
         guard let ciImage = CIImage(image: newImage) else { print("Image processing failed.Please try with another image."); return nil }
@@ -139,32 +204,6 @@ public class SemanticImage {
             return nil
         }
     }
-    
-    // MARK: Saliency
-    
-    public func saliencyMask(uiImage:UIImage) -> UIImage? {
-        let newImage = getCorrectOrientationUIImage(uiImage:uiImage)
-        guard let ciImage = CIImage(image: newImage),
-              let request = segmentationRequest else { print("Image processing failed.Please try with another image."); return nil }
-        let handler = VNImageRequestHandler(ciImage: ciImage, options: [:])
-        do {
-            try handler.perform([request])
-            guard let result = request.results?.first as? VNPixelBufferObservation
-                   else { print("Image processing failed.Please try with another image.") ; return nil }
-            let maskCIImage = CIImage(cvPixelBuffer: result.pixelBuffer)
-            let scaledMask = maskCIImage.resize(as: CGSize(width: ciImage.extent.width, height: ciImage.extent.height))
-
-            guard let safeCGImage = ciContext.createCGImage(scaledMask, from: scaledMask.extent) else { print("Image processing failed.Please try with another image.") ; return nil }
-            let maskUIImage = UIImage(cgImage: safeCGImage)
-            return maskUIImage
-
-        } catch let error {
-            print("Vision error \(error)")
-            return nil
-        }
-    }
-    
-    // MARK: Rectangle
     
     public func faceRectangles(uiImage:UIImage) -> [UIImage] {
         var faceUIImages:[UIImage] = []
